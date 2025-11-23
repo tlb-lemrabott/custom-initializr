7d529f35a13b166367f9b34d53c7e504e320777b
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-11-23 01:46:29 -0500

    feat(proxy): add Spring Boot main application

---
+++ initializr-proxy-server/src/main/java/com/initializrproxyserver/code/InitializrProxyServerApplication.java
@@ -0,0 +1,13 @@
+package com.initializrproxyserver.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrProxyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrProxyServerApplication.class, args);
+    }
+
+}
