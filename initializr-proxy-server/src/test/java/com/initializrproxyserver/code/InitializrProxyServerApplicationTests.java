b388fca19d0388cbb63d29255172ca1c517f91de
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-11-23 01:46:36 -0500

    test(proxy): add application tests skeleton

---
+++ initializr-proxy-server/src/test/java/com/initializrproxyserver/code/InitializrProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initializrproxyserver.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializrProxyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
