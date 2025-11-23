3719171876e45bca5e5173e85652d67343cdc4b2
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-11-23 01:47:59 -0500

    feat(web): add types/projectConfig

---
+++ src/types/projectConfig.ts
@@ -0,0 +1,17 @@
+export interface ProjectConfig {
+  type: string;
+  language: string;
+  bootVersion: string;
+  groupId: string;
+  artifactId: string;
+  name: string;
+  description: string;
+  packageName: string;
+  packaging: string;
+  javaVersion: string;
+  configurationFileFormat: string;
+  baseDir: string;
+  dependencies: string[];
+  backlink: string; // computed from window.location
+}
+
