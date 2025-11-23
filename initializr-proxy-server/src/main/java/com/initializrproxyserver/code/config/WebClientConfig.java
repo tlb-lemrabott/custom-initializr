b69bc5fd9c8cca79b43ba534ec20c81f172d98ee
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-11-23 01:45:37 -0500

    feat(proxy): update WebClientConfig

--- initializr-proxy-server/src/main/java/com/initializrproxyserver/code/config/WebClientConfig.java
+++ initializr-proxy-server/src/main/java/com/initializrproxyserver/code/config/WebClientConfig.java
@@ -1,4 +1,19 @@
 package com.initializrproxyserver.code.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    private static final String INITIALIZR_API = "https://start.spring.io";
+
+    @Bean
+    public WebClient initializrClient() {
+        return WebClient.builder()
+                .baseUrl(INITIALIZR_API)
+                .build();
+    }
 }
+
